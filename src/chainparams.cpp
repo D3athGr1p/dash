f9aecb6e39970167d43f13c2e75ebabf996db326
Kishan Sudani <86771928+D3athGr1p@users.noreply.github.com>
2024-02-23 15:18:38 +0530

    Development height & shares

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -203,6 +203,7 @@ public:
         consensus.nPowDGWHeight = 1;
         consensus.nRuleChangeActivationThreshold = 3; // 95% of 2016
         consensus.nMinerConfirmationWindow = 3; // nPowTargetTimespan / nPowTargetSpacing
+        consensus.DevelopementFundShare = 3;
         consensus.DevRewardStartHeight = 16700;
         consensus.DevelopmentFundAddress = "B4ZQyV266uUDFyJa3vr7D7RV9TD18Th3Dp";
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
